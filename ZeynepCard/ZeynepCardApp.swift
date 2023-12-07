3d9486fb8cc6d5dd2a6f08251564e6dbc8f42337
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2023-12-07 23:01:27 +0300

    Initial Commit

---
+++ ZeynepCard/ZeynepCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeynepCardApp.swift
+//  ZeynepCard
+//
+//  Created by Zeynep Üçüncüoğlu on 7.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZeynepCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
